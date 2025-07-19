19df3f6807c483b6d0a8641378f38ca8ce7b453b
TristanAntecolBranch <tristanantecolbranch@gmail.com>
2025-07-19 18:20:32 -0400

    test

--- puppy.go
+++ puppy.go
@@ -6,7 +6,7 @@ const sound string = "Woof!"
 
 func Bark() {
 	question := "how many times does the dog bark"
-	numTimes := "Once"
+	numTimes := "twice"
 
 	fmt.Printf("%v? %v. %v", question, numTimes, sound)
 }
